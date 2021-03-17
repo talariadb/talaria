d6d4a4d70d1d073b1af2a06e922e57ae7bec8e7f
qiao.wei <qiao.wei@grabtaxi.com>
2021-03-17 10:58:20 +0800

    upgrade orc and fix loading badger config

--- internal/config/config.go
+++ internal/config/config.go
@@ -59,7 +59,7 @@ type Table struct {
 
 // Storage is the location to write the data
 type Storage struct {
-	Badger
+	Badger `yaml:",inline"`
 	Directory string `json:"dir" yaml:"dir" env:"DIR"`
 }
 
