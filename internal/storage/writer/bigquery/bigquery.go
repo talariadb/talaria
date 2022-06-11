5a5e49f68a0810c6ab708ebb9cecaafbf594a238
JeffreyLean <leanwf1117@gmail.com>
2022-06-12 01:05:25 +0800

    change bigquery writer to use grpc api

--- internal/storage/writer/bigquery/bigquery.go
+++ internal/storage/writer/bigquery/bigquery.go
@@ -2,15 +2,25 @@ package bigquery
 
 import (
 	"context"
+	"encoding/json"
 	"runtime"
+	"time"
 
 	"cloud.google.com/go/bigquery"
+	"cloud.google.com/go/bigquery/storage/managedwriter"
+	"cloud.google.com/go/bigquery/storage/managedwriter/adapt"
 	"github.com/grab/async"
 	"github.com/kelindar/talaria/internal/encoding/block"
 	"github.com/kelindar/talaria/internal/encoding/key"
 	"github.com/kelindar/talaria/internal/monitor"
 	"github.com/kelindar/talaria/internal/monitor/errors"
 	"github.com/kelindar/talaria/internal/storage/writer/base"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protodesc"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+	"google.golang.org/protobuf/types/dynamicpb"
 )
 
 // bqRow implements ValueSaver interface Save method.
@@ -21,20 +31,26 @@ type bqRow struct {
 // Writer represents a writer for Google Cloud Storage.
 type Writer struct {
 	*base.Writer
-	dataset  string
-	table    *bigquery.Table
-	client   *bigquery.Client
-	monitor  monitor.Monitor
-	context  context.Context
-	inserter *bigquery.Inserter
-	buffer   chan []bigquery.ValueSaver
-	queue    chan async.Task
+	dataset           string
+	table             *bigquery.Table
+	client            *bigquery.Client
+	managedClient     *managedwriter.Client
+	managedStream     *managedwriter.ManagedStream
+	messageDescriptor *protoreflect.MessageDescriptor
+	monitor           monitor.Monitor
+	context           context.Context
+	queue             chan async.Task
+	buffer            chan block.Row
 }
 
 // New creates a new writer.
 func New(project, dataset, table, encoding, filter string, monitor monitor.Monitor) (*Writer, error) {
 	ctx := context.Background()
 	client, err := bigquery.NewClient(ctx, project)
+	if err != nil {
+		return nil, errors.Newf("bigquery: %v", err)
+	}
+	managedClient, err := managedwriter.NewClient(ctx, project)
 	if err != nil {
 		return nil, errors.Newf("bigquery: %v", err)
 	}
@@ -47,16 +63,30 @@ func New(project, dataset, table, encoding, filter string, monitor monitor.Monit
 	inserter := tableRef.Inserter()
 	inserter.SkipInvalidRows = true
 	inserter.IgnoreUnknownValues = true
+
+	mt, _ := tableRef.Metadata(ctx)
+	md, descriptorProto, err := setupDynamicDescriptors(mt.Schema)
+
+	ms, err := managedClient.NewManagedStream(ctx,
+		managedwriter.WithDestinationTable(managedwriter.TableParentFromParts(tableRef.ProjectID, tableRef.DatasetID, tableRef.TableID)),
+		managedwriter.WithType(managedwriter.DefaultStream),
+		managedwriter.WithSchemaDescriptor(descriptorProto),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	w := &Writer{
-		Writer:   encoderwriter,
-		dataset:  dataset,
-		table:    tableRef,
-		inserter: inserter,
-		client:   client,
-		monitor:  monitor,
-		context:  ctx,
-		buffer:   make(chan []bigquery.ValueSaver, 65000),
-		queue:    make(chan async.Task),
+		Writer:            encoderwriter,
+		dataset:           dataset,
+		table:             tableRef,
+		managedClient:     managedClient,
+		managedStream:     ms,
+		messageDescriptor: &md,
+		monitor:           monitor,
+		context:           ctx,
+		queue:             make(chan async.Task),
+		buffer:            make(chan block.Row, 100),
 	}
 	w.Process = w.process
 	return w, nil
@@ -80,27 +110,35 @@ func (w *Writer) Write(key key.Key, blocks []block.Block) error {
 	if err != nil {
 		return err
 	}
+
 	rows, _ = filtered.([]block.Row)
-	bqrows := make([]bigquery.ValueSaver, 0)
+	var result *managedwriter.AppendResult
 	for _, row := range rows {
-		bqrow := &bqRow{
-			values: row.Values,
+		v := row.Values
+		v["ingested_at"] = v["ingested_at"].(time.Time).UnixMicro()
+		j, _ := json.Marshal(v)
+		message := dynamicpb.NewMessage(*w.messageDescriptor)
+		if err = protojson.Unmarshal(j, message); err != nil {
+			return err
 		}
-		bqrows = append(bqrows, bqrow)
+		b, err := proto.Marshal(message)
+		if err != nil {
+			return err
+		}
+
+		result, err = w.managedStream.AppendRows(w.context, [][]byte{b})
+		if err != nil {
+			return err
+		}
 	}
-	if err := w.inserter.Put(w.context, bqrows); err != nil {
+	o, err := result.GetResult(w.context)
+	if err != nil {
 		return err
 	}
-	return nil
-}
-
-// Save impl for bigquery.ValueSaver interface
-func (b *bqRow) Save() (map[string]bigquery.Value, string, error) {
-	bqRow := make(map[string]bigquery.Value, len(b.values))
-	for k, v := range b.values {
-		bqRow[k] = v
+	if o != managedwriter.NoStreamOffset {
+		return errors.Newf("offset mismatch, got %d want %d", o, managedwriter.NoStreamOffset)
 	}
-	return bqRow, "", nil
+	return nil
 }
 
 // Stream publishes the rows in real-time.
@@ -116,13 +154,8 @@ func (w *Writer) Stream(row block.Row) error {
 	}
 	row, _ = filtered.(block.Row)
 
-	bqrow := &bqRow{
-		values: row.Values,
-	}
-	rows := []bigquery.ValueSaver{bqrow}
-
 	select {
-	case w.buffer <- rows:
+	case w.buffer <- row:
 	default:
 		return errors.New("bigquery: buffer is full")
 	}
@@ -133,16 +166,27 @@ func (w *Writer) Stream(row block.Row) error {
 // process will read from buffer and streams to bq
 func (w *Writer) process(parent context.Context) error {
 	async.Consume(parent, runtime.NumCPU()*8, w.queue)
-	for batch := range w.buffer {
+	for row := range w.buffer {
 		select {
 		case <-parent.Done():
 			return parent.Err()
 		default:
 		}
 		w.queue <- async.NewTask(func(ctx context.Context) (interface{}, error) {
-			err := w.inserter.Put(ctx, batch)
+			v := row.Values
+			v["ingested_at"] = v["ingested_at"].(int64) * 1000000
+			j, _ := json.Marshal(v)
+			message := dynamicpb.NewMessage(*w.messageDescriptor)
+			if err := protojson.Unmarshal(j, message); err != nil {
+				return nil, err
+			}
+			b, err := proto.Marshal(message)
 			if err != nil {
-				w.buffer <- batch
+				return nil, err
+			}
+
+			_, err = w.managedStream.AppendRows(w.context, [][]byte{b})
+			if err != nil {
 				return nil, err
 			}
 			return nil, nil
@@ -151,7 +195,31 @@ func (w *Writer) process(parent context.Context) error {
 	return nil
 }
 
+// setupDynamicDescriptors aids testing when not using a supplied proto
+func setupDynamicDescriptors(schema bigquery.Schema) (protoreflect.MessageDescriptor, *descriptorpb.DescriptorProto, error) {
+	convertedSchema, err := adapt.BQSchemaToStorageTableSchema(schema)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	descriptor, err := adapt.StorageSchemaToProto2Descriptor(convertedSchema, "root")
+	if err != nil {
+		return nil, nil, err
+	}
+	messageDescriptor, ok := descriptor.(protoreflect.MessageDescriptor)
+	if !ok {
+		return nil, nil, errors.New("adapted descriptor is not a message descriptor")
+	}
+	return messageDescriptor, protodesc.ToDescriptorProto(messageDescriptor), nil
+}
+
 // Close closes the writer.
 func (w *Writer) Close() error {
-	return w.client.Close()
+	if err := w.managedClient.Close(); err != nil {
+		return err
+	}
+	if err := w.client.Close(); err != nil {
+		return err
+	}
+	return nil
 }
